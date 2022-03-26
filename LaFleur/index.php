a1d19c59fb02b0fd89188cf63b139ad66cc81703
phprom2 <100920245+phprom2@users.noreply.github.com>
2022-03-26 23:27:23 +0100

    Added objectives 1,2,3 of the sprint 0
    Page d'accueil
    Liste des produit relié à la BDD
    Barre de recherche fonctionnel

--- LaFleur/index.php
+++ LaFleur/index.php
@@ -4,10 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>
+        Document
+    </title>
 </head>
 <body>
-    <h1>test</h1>
-    
+<h1>
+    Liste des catégories
+</h1>
+<div class="container">
+  <div class="row">
+    <div class="col-10">
+        <li>
+            <a href="Bulbes.php">
+                Bulbles
+            </a>
+        </li>
+        <li>
+            <a href="Massifs.php">
+                Massifs
+            </a>
+        </li>
+        <li>
+            <a href="Rosiers.php">
+                Rosiers
+            </a>
+        </li>
+    </div>
+    <div class="col-2">
+        <form action="recherche.php" method="GET">
+            <label for="Votre Recherche" class="form-label">Votre recherche</label>
+            <input class="form-control" name="recherche" id="recherche">
+            <button type="submit" class="btn btn-primary">Rechercher</button>
+        </form>
+    </div>
+  </div>
+</div>
 </body>
 </html>
